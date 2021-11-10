2457199f42e43428be1eb7f71de41f7bdc58d18c
Meowmori Aoi <meowmoriaoi@gmail.com>
2021-11-10 20:50:20 +0700

    Setup Tailwindcss for Express Project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    './public/**/*.html',
+    './views/**/*.pug'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
